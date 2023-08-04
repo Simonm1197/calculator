50d0e7f98f7b51221ac740040726abd74d9214a6
Simonm1197 <119571445+Simonm1197@users.noreply.github.com>
2023-08-04 17:02:25 +0530

    completed

---
+++ App.tsx
@@ -0,0 +1,13 @@
+// import {SafeAreaView} from 'react-native';
+import React from 'react';
+import Calsi from './src/Calsi';
+
+const App = () => {
+  return (
+    <>
+      <Calsi />
+    </>
+  );
+};
+
+export default App;
